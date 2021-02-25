b112d7eae2ea3b0dca774556eaa59aac6f525a19
Amy Hogan <akhogan@ucsd.edu>
2021-02-24 19:02:30 -0800

    thirdpartyAPI lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,12 @@
 function initMap() {
-	// add your code here
+	L.mapquest.key = 'aADfnwOTJY7XvG5pGNqMflobP8pKER2A';
+
+	var map = L.mapquest.map('map', {
+  		center: [32.87873652854944, -117.23591818889847],
+  		layers: L.mapquest.tileLayer('map'),
+  		zoom: 12
+	});
+
+	//add UCSD marker to map
+	L.marker([32.87873652854944, -117.23591818889847]).addTo(map);
 }
--- routes/rsvp.js
+++ routes/rsvp.js
@@ -5,6 +5,19 @@ var data = {
 /*
  * GET tasks page.
  */
+
+exports.addRSVP = function(request, response){
+var rsvpEmail = request.body.rsvpEmail;
+	// Will print to terminal whenever POST is made
+	console.log(rsvpEmail);
+
+	data.rsvp.push(rsvpEmail); // Add to current data
+
+	// Send back data so browser knows it worked
+	response.send(rsvpEmail);
+};
+
+
 exports.adminView = function(request, response){
   response.render('rsvp', data);
 };
